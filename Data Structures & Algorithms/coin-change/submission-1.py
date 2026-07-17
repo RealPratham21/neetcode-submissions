5cf9fb26fa5d3944d00ce06cc278f50ca925ebbe
Prathamesh <157454031+RealPratham21@users.noreply.github.com>
2026-07-17 18:07:03 +0530

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        coins.sort(reverse=True)
+        n = len(coins)
+
+        def dp(pos, remain):
+            if remain == 0:
+                return 0
+            
+            if pos >= n:
+                return float('inf')
+            
+            min_moves = float('inf')
+
+            if remain >= coins[pos]:
+                take = dp(pos + 1, remain % coins[pos]) + (remain // coins[pos])
+            else:
+                take = float('inf')
+
+            skip = dp(pos + 1, remain)
+
+            return min(take, skip)
+        
+        res = dp(0, amount)
+
+        return res if res != float('inf') else -1
