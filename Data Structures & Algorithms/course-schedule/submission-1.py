105c0231cc57a4915f4de9441040e660f730d13b
Prathamesh <157454031+RealPratham21@users.noreply.github.com>
2026-07-25 17:39:24 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        dep_graph = defaultdict(list)
+        dependent = set()
+
+        for u, v in prerequisites:
+            dep_graph[u].append(v)
+            dependent.add(v)
+
+        dfs = []
+
+        for i in range(numCourses):
+            if i not in dependent:
+                dfs.append(i)
+        
+        visited = set()
+
+        while dfs:
+            curr_node = dfs.pop()
+
+            visited.add(curr_node)
+
+            if len(visited) == numCourses:
+                return True
+            
+            for i in dep_graph[curr_node]:
+                if i not in visited:
+                    dfs.append(i)
+            
+        return False
